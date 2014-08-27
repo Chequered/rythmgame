92cb43dee0110361c72f3431aab3fadc709aa5e2
Niek Schoone <16789@ma-web.nl>
2014-08-27 09:44:15 +0200

    artPart

---
+++ project/Assets/Script/BackgroundScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScript : MonoBehaviour {
+
+	public float scrollSpeed;
+	public float offset;
+	
+	// Update is called once per frame
+	void Update () {
+		offset += scrollSpeed;
+		renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+		if (offset >= 1) 
+		{
+			Debug.Log(offset);
+			offset = -1;
+			renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+		}
+	}
+}
