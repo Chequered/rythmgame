cb593cf041c9cc4e5c2dd3cf9b04dfecd8daace2
jeremybond <jeremybond2@hotmail.com>
2014-08-27 18:40:27 +0200

    StartScreen CreditsScreen DeadScreen

---
+++ project/Assets/Script/GUI/StartScreenGUI/CreditsButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsButton : MonoBehaviour {
+
+	void Awake(){
+		guiTexture.texture = button1;
+	}
+	public Texture2D button1;
+	public Texture2D button2;
+	public GameObject creditsGUI;
+	void OnMouseDown(){
+		guiTexture.texture = button2;
+	}
+	void OnMouseUp(){
+		Vector2 mousepos = Input.mousePosition;
+		if(guiTexture.HitTest(mousepos))
+		{
+			ButtonPushed();
+		}
+		guiTexture.texture = button1;
+	}
+	public void ButtonPushed(){
+		creditsGUI.GetComponent<CreditsScreenCommands>().GoCredits();
+	}
+}
---
+++ project/Assets/Script/GUI/StartScreenGUI/CreditsScreenCommands.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScreenCommands : MonoBehaviour {
+
+	float relativeForce = 0.2f;
+	public void GoCredits()
+	{
+		StartCoroutine(CreditsButtonPushed());
+	}
+	IEnumerator CreditsButtonPushed(){
+		Debug.Log("something is happening");
+		if(transform.position.x <= -0.2f)
+		{
+			transform.position += new Vector3(relativeForce, 0, 0);
+			yield return new WaitForSeconds(0.00002f);
+			StartCoroutine(CreditsButtonPushed());
+		}
+	}
+	public void ExitCredits(){
+		StartCoroutine(ExitCreditsButtonPushed());
+	}
+	IEnumerator ExitCreditsButtonPushed(){
+		if(transform.position.x >= -15f)
+		{
+			transform.position -= new Vector3(relativeForce, 0, 0);
+			yield return new WaitForSeconds(0.00002f);
+			StartCoroutine(ExitCreditsButtonPushed());
+		}
+	}
+}
