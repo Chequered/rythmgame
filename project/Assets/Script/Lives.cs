cd82e6e8724128d56085125088b6d0c339c1d4d2
Daan Ruiter <daanruiter@live.nl>
2014-08-29 14:31:48 +0200

    update

--- project/Assets/Script/Lives.cs
+++ project/Assets/Script/Lives.cs
@@ -9,12 +9,8 @@ public class Lives : MonoBehaviour {
 
 	public void UpdateLives(int change){
 		GameController.lives -= change;
-<<<<<<< HEAD
 		if(GameController.lives == 0){
-=======
-		if(GameController.lives < 0){
 			loseScreenGUI.GetComponent<LoseScreen>().Lost();
->>>>>>> 4c03c0f62a7c311b911ed93a47e55f2d45247878
 			GameController.status = true;
 		}
 		guiText.text = "Lives: " + GameController.lives;
